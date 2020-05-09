a2b2cbe4fa20f3c6b4744bed51c74700e9908b29
Ricardo Moreno <ricky.gnr.moreno@gmail.com>
2020-05-09 17:56:14 -0300

    Improve header style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import './App.css';
-import Main from './components/Main';
+import React, { Component } from "react";
+import "./App.css";
+import Main from "./components/Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Main/>
-        
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Ricky'
+    }
+  }
+
+  render() {
+
+    const {userName} =  this.state;
+    return (
+      <div className="App">
+        <header className="App-container">
+          <Main userName={userName} />
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
